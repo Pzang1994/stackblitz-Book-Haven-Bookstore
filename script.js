fdf287f635fd0b3acb8e53fcb3c1e55b9c054813
Pzang1994 <zangp1994@gmail.com>
2024-12-04 15:28:27 -0500

    Fix function displayMenu() - Now properly evaluates display of "links" - hamburgerMenu now functions as intended

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+const log = console.log;
+
+let hamburgerMenu = document.getElementById("hamburger-menu");
+let links = document.getElementById("hamburger-links");
+
+function displayMenu(){
+    if (links.style.display === "none"){
+        links.style.display = "block";
+        log("this happened");
+    } else { 
+        links.style.display = "none";
+        log("then this happened");
+    };
+};
+
+hamburgerMenu.addEventListener("click", displayMenu);
