031a0ac4bd9d8c53851f8478d46ffe9e411c9822
Pzang1994 <zangp1994@gmail.com>
2024-12-04 20:30:43 -0500

    Set Body padding and margin to 0px; Set Font settings throughout body; Add styles to Banner on Homepage; Set Hamburger menu display to none when screen is larger; Create placeholder for searchbar to move to when screen is larger; Create function in js to move searchbar when screen is larger and move searchbar again when screen is smaller; Set background caller of searchbar to change when searchbar moves; Set searchbar and container div to flex: auto when searchbar moves

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const log = console.log;
 
 let hamburgerMenu = document.getElementById("hamburger-menu");
 let links = document.getElementById("hamburger-links");
+let placeHolder1 = document.getElementById("placeholder-1");
+let placeHolder2 = document.getElementById("placeholder-2");
+let searchBar = document.getElementById("searchbar");
 
 function displayMenu(){
     if (links.style.display === "none"){
@@ -14,3 +17,19 @@ function displayMenu(){
 };
 
 hamburgerMenu.addEventListener("click", displayMenu);
+
+function moveSearchbar(mediaQuery) {
+    if (mediaQuery.matches) {
+        placeHolder1.appendChild(searchBar);
+        console.log("Minimum width reached!");
+    } else if (placeHolder1.hasChildNodes()){
+        placeHolder2.appendChild(searchBar);
+        console.log("Below minimum width.");
+    }
+  }
+  
+  const minWidthMediaQuery = window.matchMedia("(min-width: 480px)");
+  
+moveSearchbar(minWidthMediaQuery);
+
+minWidthMediaQuery.addEventListener("change", moveSearchbar);
